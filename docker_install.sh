7612fb10809819ad71aa57d9e256c0ad638e075a
Saifullah Shams <133542218+saifullahshams2@users.noreply.github.com>
2024-09-20 00:31:03 +0300

    Add Auto Docker Install

---
+++ docker_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Update & Upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install Portainer for Docker Portainer will be in Port :9000
+
+docker volume create portainer_data
+docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
