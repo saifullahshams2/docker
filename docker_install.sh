aa396fd0fda213eb4f91a16c1ddb0def0c381e50
Saifullah Shams <133542218+saifullahshams2@users.noreply.github.com>
2024-09-23 00:57:41 +0300

    up

--- docker_install.sh
+++ docker_install.sh
@@ -5,8 +5,6 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 sudo apt install docker.io -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl start docker
