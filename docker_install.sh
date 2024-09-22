3a16388eb2698ff05016c6b6ae17e43c2642ecde
Saifullah Shams <133542218+saifullahshams2@users.noreply.github.com>
2024-09-22 19:07:42 +0300

    Add Caddy

--- docker_install.sh
+++ docker_install.sh
@@ -20,3 +20,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 docker volume create portainer_data
 docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# Install Caddy Docker
+docker network create -d bridge caddy-net
+docker volume create caddy_data
+docker volume create caddy_config
+docker run --net caddy-net --name caddy --restart always -p 80:80 -p 443:443 -p 443:443/udp -v $PWD/Caddyfile:/etc/caddy/Caddyfile -v $PWD/site:/srv -v $PWD/caddy_data:/data -v $PWD/caddy_config:/config caddy:latest
