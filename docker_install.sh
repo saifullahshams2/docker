f29783361b74629d0ad7599cd711918a811cbccb
Saifullah Shams <133542218+saifullahshams2@users.noreply.github.com>
2024-09-23 01:34:13 +0300

    Update

--- docker_install.sh
+++ docker_install.sh
@@ -17,10 +17,16 @@ sudo apt update && sudo apt upgrade -y
 # sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 # sudo systemctl start docker
 # sudo systemctl enable docker
+#
 # Install Portainer for Docker Portainer will be in Port :9000
 docker volume create portainer_data
 docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#
 # Install Caddy Docker
+cd ~
+mkdir caddy
+cd caddy
+touch Caddyfile
 docker network create -d bridge caddy-net
 docker volume create caddy_data
 docker volume create caddy_config
