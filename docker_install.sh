884c6afa481ba292bd0d77e1ae67bc43737f32ae
Saifullah Shams <133542218+saifullahshams2@users.noreply.github.com>
2024-10-15 00:58:44 +0300

    docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -9,7 +9,5 @@ mkdir caddy
 cd caddy
 touch Caddyfile
 docker network create -d bridge caddy-net
-docker volume create caddy_data
-docker volume create caddy_config
 docker run --net caddy-net --name caddy --restart always -p 80:80 -p 443:443 -p 443:443/udp -v $PWD/Caddyfile:/etc/caddy/Caddyfile -v $PWD/site:/srv -v $PWD/caddy_data:/data -v $PWD/caddy_config:/config caddy:latest
 docker network connect caddy-net portainer
